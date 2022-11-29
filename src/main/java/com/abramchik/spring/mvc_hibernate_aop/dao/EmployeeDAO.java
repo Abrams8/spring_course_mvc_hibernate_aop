44c883f5ae27e38c7d9a2bd51b5ddd8fd8f0fd40
Dima Abramchik <dima.abramchik@gmail.com>
2022-11-30 02:09:58 +0300

    Initial commit

---
+++ src/main/java/com/abramchik/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.abramchik.spring.mvc_hibernate_aop.dao;
+
+import com.abramchik.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
